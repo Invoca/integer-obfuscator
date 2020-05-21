0395c15c516848864c4ef8ff71cbbbbb9b9bcb90
Rachel Pham <rpham@invoca.com>
2020-05-21 15:45:04 -0700

    TECH-4484: add formatters

--- test/helper.rb
+++ test/helper.rb
@@ -1,6 +1,10 @@
 require 'rubygems'
 require 'test/unit'
 require 'minitest/reporters'
+Minitest::Reporters.use! [
+  Minitest::Reporters::DefaultReporter.new,
+  Minitest::Reporters::JUnitReporter.new
+]
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 
