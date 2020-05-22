b0d5ed6d1ff3e162c61925114f6a531268269782
Nick Burwell <nick@invoca.com>
2020-05-21 23:32:38 -0700

    TECH-4484: get test reporting to work

--- test/helper.rb
+++ test/helper.rb
@@ -1,5 +1,6 @@
-require 'rubygems'
-require 'test/unit'
+# frozen_string_literal: true
+
+require 'minitest/autorun'
 require 'minitest/reporters'
 Minitest::Reporters.use! [
   Minitest::Reporters::DefaultReporter.new,
--- test/test_integer_obfuscator.rb
+++ test/test_integer_obfuscator.rb
@@ -1,42 +1,48 @@
-require "helper"
+# frozen_string_literal: true
 
-class TestIntegerObfuscator < Test::Unit::TestCase
-  
-  def setup
-    @c = IntegerObfuscator.new("my secret key")
-  end
-  
-  def test_obfuscation
-    assert_equal 418026769, @c.obfuscate(1)
-  end
-  
-  def test_unobfuscation
-    assert_equal 1, @c.unobfuscate(418026769)
-  end
-  
-  def test_1000_obfuscation_deobfuscates
-    0.upto(1000) do |i|
-      assert_equal i, @c.unobfuscate(@c.obfuscate(i))
-    end
-  end
-  
-  def test_does_not_allow_negative_values
-    assert_raises IntegerObfuscator::RangeError do
-      @c.obfuscate(-1)
-    end
-    assert_raises IntegerObfuscator::RangeError do
-      @c.unobfuscate(-1)
-    end
-  end
-  
-  def test_does_not_allow_big_integers
-    bigint = (2 ** 32) + 1
-    assert_raises IntegerObfuscator::RangeError do
-      @c.obfuscate(bigint)
-    end
-    assert_raises IntegerObfuscator::RangeError do
-      @c.unobfuscate(bigint)
-    end
-  end
+require_relative "helper"
 
+describe IntegerObfuscator do
+  let(:subject) { IntegerObfuscator.new("my secret key") }
+  
+  describe '#obfuscate' do
+    it 'works' do
+      assert_equal 418026769, subject.obfuscate(1)
+    end
+  end
+  
+  describe '#unobfuscation' do
+    it 'works' do
+      assert_equal 1, subject.unobfuscate(418026769)
+    end
+  
+    it 'works up to 1000' do
+      0.upto(1000) do |i|
+        assert_equal i, subject.unobfuscate(subject.obfuscate(i))
+      end
+    end
+  end
+  
+  describe 'negative values' do
+    it 'does not allow' do
+      assert_raises IntegerObfuscator::RangeError do
+        subject.obfuscate(-1)
+      end
+      assert_raises IntegerObfuscator::RangeError do
+        subject.unobfuscate(-1)
+      end
+    end
+  end
+  
+  describe 'big integers' do
+    it 'does not allow' do
+      bigint = (2 ** 32) + 1
+      assert_raises IntegerObfuscator::RangeError do
+        subject.obfuscate(bigint)
+      end
+      assert_raises IntegerObfuscator::RangeError do
+        subject.unobfuscate(bigint)
+      end
+    end
+  end
 end
